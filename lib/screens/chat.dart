3f3f6daf82b128af643b095feefac2bab3514b21
adamWinebarger <adam.k.winebarger@gmail.com>
2023-08-08 21:33:31 -0400

    Smoothed over the compatibility issues with Flutter, Firebase, iOS, and MacOS. Now I'm just updating so I can test my work on the android emulator as well

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,30 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void _setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token = fcm.getToken();
+    print(token); //needed to be able to get our push notifs.
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setupPushNotifications();
+  }
 
   @override
   Widget build(context) {
@@ -33,5 +53,4 @@ class ChatScreen extends StatelessWidget {
       ),
     );
   }
-
 }
